4f18b7a001963f31edcb15ce91ffacbf89fb6424
Nicholas <nick.orsi1992@gmail.com>
2024-01-17 16:06:54 -0800

    Added tests for more wrong guess than the max guess allowed.

--- src/Snowman.test.js
+++ src/Snowman.test.js
@@ -2,13 +2,16 @@ import { render, fireEvent } from "@testing-library/react";
 import Snowman from "./Snowman";
 
 const TEST_WORDS = ["TEST"];
+const TEST_MAX_GUESS = 3;
 
 test("renders without crashing", function () {
-  render (<Snowman words={TEST_WORDS} />)
+  render (<Snowman words={TEST_WORDS} maxWrong={TEST_MAX_GUESS} />)
 });
 
 test("shows number wrong", function () {
-  const { container } = render (<Snowman words={TEST_WORDS} />);
+  const { container } = render (
+    <Snowman words={TEST_WORDS} maxWrong={TEST_MAX_GUESS}/>
+  );
 
   const aButton = container.querySelector('button[value="a"]');
   fireEvent.click(aButton);
@@ -19,4 +22,25 @@ test("shows number wrong", function () {
   const bButton = container.querySelector('button[value="b"]');
   fireEvent.click(bButton);
   expect(nWrongText).toContainHTML('Number wrong: 2')
+});
+
+test("can't have more nWrong than maxWrong", function () {
+  const { container } = render (
+    <Snowman words={TEST_WORDS} maxWrong={TEST_MAX_GUESS}/>
+  );
+
+  const aButton = container.querySelector('button[value="a"]');
+  const bButton = container.querySelector('button[value="b"]');
+  const cButton = container.querySelector('button[value="c"]');
+
+  fireEvent.click(aButton);
+  fireEvent.click(bButton);
+  fireEvent.click(cButton);
+
+  const dButton = container.querySelector('button[value="d"]');
+  const img = container.querySelector('img[alt="3"]');
+
+  expect(dButton).toEqual(null);
+  expect(container).toContainHTML(`You lose, correct word is ${TEST_WORDS[0]}`);
+  expect(img).not.ßtoEqual(null);
 });
